3a939c93643865741bfe306669b5e2b60771df69
Seongmoon Kim <tokyokim0226@naver.com>
2023-12-25 16:49:44 +0900

    button created for each choice for Q

---
+++ Javascript/js30dayschallenge/3_simpleQuizApp/script.js
@@ -0,0 +1,54 @@
+const questions = [
+    {
+        question: "What is the largest animal in the world?",
+        answers: [
+            { text: "Shark", correct: false},
+            { text: "Blue Whale", correct: true},
+            { text: "Elephant", correct: false},
+            { text: "Giraffe", correct: false},
+        ]
+    },
+    {
+        question: "What is the heaviest animal on land?",
+        answers: [
+            { text: "Shark", correct: false},
+            { text: "Blue Whale", correct: false},
+            { text: "Elephant", correct: true},
+            { text: "Giraffe", correct: false},
+        ]
+    }
+];
+
+const questionElement = document.getElementById("question");
+//BIGGEST TAKEAWAY
+// DO NOT MIX UP WHETHER IT IS A CLASS OR A ID --> only give class for when
+// different elements can possess the same clas
+const answerButton = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+// variables to store current question number and the score (# of questions answerred correctly)
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz() {
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+
+function showQuestion() {
+    let currentQuestion = questions[currentQuestionIndex];
+    console.log(currentQuestion);
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButton.appendChild(button);
+    });
+}
+
+startQuiz();
