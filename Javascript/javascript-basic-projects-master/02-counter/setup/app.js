01e893dc82144b0a27224e6c476118b08b9afd39
Seongmoon Kim <tokyokim0226@naver.com>
2023-12-28 11:34:57 +0900

    project 3 in progress

--- Javascript/javascript-basic-projects-master/02-counter/setup/app.js
+++ Javascript/javascript-basic-projects-master/02-counter/setup/app.js
@@ -17,6 +17,13 @@ btns.forEach(function(btn) {
         }else{
             count = 0;
         }
+        if (count>0) {
+            value.style.color = "green";
+        }else if (count <0) {
+            value.style.color = "red";
+        }else{
+            value.style.color = "black"
+        }
         value.textContent = count;
     })
 })
