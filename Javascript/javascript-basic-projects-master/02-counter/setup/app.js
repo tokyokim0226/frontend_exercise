666419bc32ce4ebbee13a44d9d77a2c64d420b68
Seongmoon Kim <tokyokim0226@naver.com>
2023-12-28 11:18:54 +0900

    02 if statements

---
+++ Javascript/javascript-basic-projects-master/02-counter/setup/app.js
@@ -0,0 +1,22 @@
+//set initial count
+let count = 0;
+
+//select value and buttons
+const value = document.querySelector('#value');
+const btns = document.querySelectorAll('.btn');
+
+console.log(btns);
+
+btns.forEach(function(btn) {
+    btn.addEventListener('click', function(e) {
+        const styles = e.target.classList;
+        if(styles.contains('decrease')){
+            count --;
+        }else if(styles.contains('increase')) {
+            count ++;
+        }else{
+            count = 0;
+        }
+        value.textContent = count;
+    })
+})
