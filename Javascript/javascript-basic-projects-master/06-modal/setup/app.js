001b45feefa31f89573c7bd108f709786c950432
Seongmoon Kim <tokyokim0226@naver.com>
2023-12-28 15:59:45 +0900

    06 modal (overlay tab) complete

--- Javascript/javascript-basic-projects-master/06-modal/setup/app.js
+++ Javascript/javascript-basic-projects-master/06-modal/setup/app.js
@@ -2,3 +2,22 @@
 // listen for click events on modal-btn and close-btn
 // when user clicks modal-btn add .open-modal to modal-overlay
 // when user clicks close-btn remove .open-modal from modal-overlay
+const openModalBtn = document.querySelector('.modal-btn');
+const modalOverlay = document.querySelector(".modal-overlay");
+const closeBtn = document.querySelector(".close-btn");
+
+//checking to see if the constants declared above are properly selected
+// console.log(closeBtn);
+// console.log(openModalBtn);
+// console.log(modalOverlay);
+
+openModalBtn.addEventListener("click", () => {
+    //open the modal overlay
+    modalOverlay.classList.add("open-modal"); //can also use toggle
+})
+
+
+closeBtn.addEventListener("click", function(){
+    //hide the modal overlay
+    modalOverlay.classList.remove("open-modal");
+})
