eb2114df5c7a9069430923577488e7bef5077f56
Seongmoon Kim <tokyokim0226@naver.com>
2024-01-02 09:36:05 +0900

    testing for update

--- Javascript/javascript-basic-projects-master/08-menu/setup/app.js
+++ Javascript/javascript-basic-projects-master/08-menu/setup/app.js
@@ -7,7 +7,7 @@ const menu = [
     id: 1,
     title: "buttermilk pancakes",
     category: "breakfast",
-    price: 15.99,
+    price: 15.98,
     img: "./images/item-1.jpeg",
     desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
   },
