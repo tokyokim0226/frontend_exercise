6a7eede8cf6c36f76ae36da7a3427cf04b85b615
Seongmoon Kim <tokyokim0226@naver.com>
2024-01-15 09:32:41 +0900

    finished Menu project 8

--- Javascript/javascript-basic-projects-master/08-menu/setup/app.js
+++ Javascript/javascript-basic-projects-master/08-menu/setup/app.js
@@ -1,3 +1,9 @@
+// get only the unique categories - hardest step
+// iterate over categories and return buttons
+// make sure to select buttons when they are available
+
+
+
 const menu = [
   {
     id: 1,
@@ -71,32 +77,70 @@ const menu = [
     img: "./images/item-9.jpeg",
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
+  {
+    id: 9,
+    title: "steak dinner",
+    category: "dinner",
+    price: 39.99,
+    img: "./images/item-10.jpeg",
+    desc: `new steak menu fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+  }
 ];
 
 const sectionCenter = document.querySelector(".section-center");
-const filterBtns = document.querySelectorAll(".filter-btn");
+const container = document.querySelector('.btn-container');
 
 //load items
 window.addEventListener("DOMContentLoaded", function () {
-  displayMenuItems(menu);
-});
+  displayMenuItems(menu); //right after displaying items, iterate through buttons (dynamically)
 
-//filter items
-filterBtns.forEach(function(btn) {
-  btn.addEventListener('click', function(e) {
-    const category = e.currentTarget.dataset.id;
-    const menuCategory = menu.filter(function(menuItem) {
-      if(menuItem.category === category) {
-        return menuItem;
+  const categories = menu.reduce(function(values, item){
+    if(!values.includes(item.category)){
+      values.push(item.category);
+    }
+    //when using reduce, must always return values
+    return values
+  }, ['all']);
+  const categoryBtns = categories.map(function(category) {
+    return `<button class="filter-btn" type = "button" data-id=${category}>${category}</button>`
+  }).join("");
+  container.innerHTML = categoryBtns;
+  const filterBtns = document.querySelectorAll(".filter-btn");
+  //filter items
+  filterBtns.forEach(function(btn) {
+    btn.addEventListener('click', function(e) {
+      const category = e.currentTarget.dataset.id;
+      const menuCategory = menu.filter(function(menuItem) {
+        if(menuItem.category === category) {
+          return menuItem;
+        }
+      });
+      if (category === 'all') {
+        displayMenuItems(menu);
+      }else {
+        displayMenuItems(menuCategory);
       }
     });
-    if (category === 'all') {
-      displayMenuItems(menu);
-    }else {
-      displayMenuItems(menuCategory);
-    }
-  })
-})
+  });
+});
+
+// now placed inside DOMContentLoaded 
+//filter items
+// filterBtns.forEach(function(btn) {
+//   btn.addEventListener('click', function(e) {
+//     const category = e.currentTarget.dataset.id;
+//     const menuCategory = menu.filter(function(menuItem) {
+//       if(menuItem.category === category) {
+//         return menuItem;
+//       }
+//     });
+//     if (category === 'all') {
+//       displayMenuItems(menu);
+//     }else {
+//       displayMenuItems(menuCategory);
+//     }
+//   });
+// });
 
 
 
@@ -118,7 +162,6 @@ function displayMenuItems(menuItems){
         </article>`;
   });
   displayMenu = displayMenu.join("");
-  console.log(displayMenu);
 
   sectionCenter.innerHTML = displayMenu;
 }
